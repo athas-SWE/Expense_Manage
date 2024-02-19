773539e43ad430b48615d420cd9ab9319a92fc45
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-19 23:48:06 +0530

    first

--- Expenses_Management/Controllers/ExpensesController.cs
+++ Expenses_Management/Controllers/ExpensesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expenses.DataAccess.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Expenses_Management.Controllers
 {
 	public class ExpensesController : Controller
+
 	{
+		private readonly IExpensesRepository _expense;
 		public IActionResult Index()
 		{
 			return View();
