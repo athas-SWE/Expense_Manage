314b7e909f3606bd63e98f32ee69d4459409e44e
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-19 22:19:57 +0530

    first

--- Expenses_Management/Program.cs
+++ Expenses_Management/Program.cs
@@ -1,9 +1,18 @@
+using Expenses.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// DB Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
