fa8e1a31b270e331132b8bb9c6ba17a933ede654
Athas @ara <83549525+it20301422@users.noreply.github.com>
2024-02-19 22:50:06 +0530

    first

---
+++ Expenses.DataAccess/Repositories/ExpensesRepository.cs
@@ -0,0 +1,43 @@
+﻿using Expenses.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expenses.DataAccess.Repositories
+{
+	public class ExpensesRepository : IExpensesRepository
+	{
+		private readonly ApplicationDbContext _context;
+		public void Add(ExpenseModel expense)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ExpenseModel> GetAllExpenses()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ExpenseModel GetExpenseById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<ExpenseModel> Search(string searchString)
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Update(ExpenseModel expense)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- Expenses.DataAccess/Repositories/IExpensesRepository.cs
+++ Expenses.DataAccess/Repositories/IExpensesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Expenses.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Expenses.DataAccess.Repositories
 {
 	public interface IExpensesRepository
 	{
+		IEnumerable<ExpenseModel> GetAllExpenses();
+
+		IEnumerable<ExpenseModel> Search(string searchString);
+
+		void Add(ExpenseModel expense);
+
+		int Update(ExpenseModel expense);
+
+		ExpenseModel GetExpenseById(int id);
+
+		void Delete(int id);
 	}
 }
